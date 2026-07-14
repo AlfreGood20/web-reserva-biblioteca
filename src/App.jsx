bd603e279702e80ccf1b4e042e31f797dd79731a
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-14 15:59:17 -0600

    LOGO BIBLIOTECA - ME

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
 
           <Route path='/' element={<Home />}/>
           <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
 
         </Routes>
 
