93905ea83cf95e6bbc818851dd91ea6a17e8a862
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-23 14:26:52 -0600

    PAGINAS QUE TENDRA NAVBAR Y CUALES NO

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,26 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import MyReservations from './pages/MyReservations'
+import Layout from './components/Layout'
 
 function App() {
   
   return (
     <>
-      <ToastContainer />
+      <BrowserRouter>     
+        <ToastContainer />
 
-      <BrowserRouter>
+        {/* ESTOS COMPONENTES VAN A TENER NAVBAR */}
+        <Routes> 
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />}/>
+            <Route path='/my-reservations' element={<MyReservations />} />
+        </Route>
 
-        <Routes>
-
-          <Route path='/' element={<Home />}/>
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
-
+         
         </Routes>
 
       </BrowserRouter>
